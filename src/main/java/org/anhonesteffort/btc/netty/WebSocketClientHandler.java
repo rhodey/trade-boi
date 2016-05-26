98ee41f909ed352bc55f8e2da48d3c71cf1d4143
Rhodey Orbits <rhodey@anhonesteffort.org>
2016-05-25 20:08:02 -0500

    format

--- src/main/java/org/anhonesteffort/btc/netty/WebSocketClientHandler.java
+++ src/main/java/org/anhonesteffort/btc/netty/WebSocketClientHandler.java
@@ -52,67 +52,67 @@ import io.netty.util.CharsetUtil;
 
 public class WebSocketClientHandler extends SimpleChannelInboundHandler<Object> {
 
-    private final WebSocketClientHandshaker handshaker;
-    private ChannelPromise handshakeFuture;
+  private final WebSocketClientHandshaker handshaker;
+  private ChannelPromise handshakeFuture;
 
-    public WebSocketClientHandler(WebSocketClientHandshaker handshaker) {
-        this.handshaker = handshaker;
+  public WebSocketClientHandler(WebSocketClientHandshaker handshaker) {
+    this.handshaker = handshaker;
+  }
+
+  public ChannelFuture handshakeFuture() {
+    return handshakeFuture;
+  }
+
+  @Override
+  public void handlerAdded(ChannelHandlerContext ctx) {
+    handshakeFuture = ctx.newPromise();
+  }
+
+  @Override
+  public void channelActive(ChannelHandlerContext ctx) {
+    handshaker.handshake(ctx.channel());
+  }
+
+  @Override
+  public void channelInactive(ChannelHandlerContext ctx) {
+    System.out.println("WebSocket Client disconnected!");
+  }
+
+  @Override
+  public void messageReceived(ChannelHandlerContext ctx, Object msg) {
+    Channel ch = ctx.channel();
+    if (!handshaker.isHandshakeComplete()) {
+      handshaker.finishHandshake(ch, (FullHttpResponse) msg);
+      System.out.println("WebSocket Client connected!");
+      handshakeFuture.setSuccess();
+      return;
     }
 
-    public ChannelFuture handshakeFuture() {
-        return handshakeFuture;
+    if (msg instanceof FullHttpResponse) {
+      FullHttpResponse response = (FullHttpResponse) msg;
+      throw new IllegalStateException(
+          "Unexpected FullHttpResponse (getStatus=" + response.status() +
+              ", content=" + response.content().toString(CharsetUtil.UTF_8) + ')');
     }
 
-    @Override
-    public void handlerAdded(ChannelHandlerContext ctx) {
-        handshakeFuture = ctx.newPromise();
+    WebSocketFrame frame = (WebSocketFrame) msg;
+    if (frame instanceof TextWebSocketFrame) {
+      TextWebSocketFrame textFrame = (TextWebSocketFrame) frame;
+      System.out.println("WebSocket Client received message: " + textFrame.text());
+    } else if (frame instanceof PongWebSocketFrame) {
+      System.out.println("WebSocket Client received pong");
+    } else if (frame instanceof CloseWebSocketFrame) {
+      System.out.println("WebSocket Client received closing");
+      ch.close();
     }
+  }
 
-    @Override
-    public void channelActive(ChannelHandlerContext ctx) {
-        handshaker.handshake(ctx.channel());
+  @Override
+  public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+    cause.printStackTrace();
+    if (!handshakeFuture.isDone()) {
+      handshakeFuture.setFailure(cause);
     }
-
-    @Override
-    public void channelInactive(ChannelHandlerContext ctx) {
-        System.out.println("WebSocket Client disconnected!");
-    }
-
-    @Override
-    public void messageReceived(ChannelHandlerContext ctx, Object msg) {
-        Channel ch = ctx.channel();
-        if (!handshaker.isHandshakeComplete()) {
-            handshaker.finishHandshake(ch, (FullHttpResponse) msg);
-            System.out.println("WebSocket Client connected!");
-            handshakeFuture.setSuccess();
-            return;
-        }
-
-        if (msg instanceof FullHttpResponse) {
-            FullHttpResponse response = (FullHttpResponse) msg;
-            throw new IllegalStateException(
-                    "Unexpected FullHttpResponse (getStatus=" + response.status() +
-                            ", content=" + response.content().toString(CharsetUtil.UTF_8) + ')');
-        }
-
-        WebSocketFrame frame = (WebSocketFrame) msg;
-        if (frame instanceof TextWebSocketFrame) {
-            TextWebSocketFrame textFrame = (TextWebSocketFrame) frame;
-            System.out.println("WebSocket Client received message: " + textFrame.text());
-        } else if (frame instanceof PongWebSocketFrame) {
-            System.out.println("WebSocket Client received pong");
-        } else if (frame instanceof CloseWebSocketFrame) {
-            System.out.println("WebSocket Client received closing");
-            ch.close();
-        }
-    }
-
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-        cause.printStackTrace();
-        if (!handshakeFuture.isDone()) {
-            handshakeFuture.setFailure(cause);
-        }
-        ctx.close();
-    }
+    ctx.close();
+  }
 }
